da116766ccf50c271eab8216e53f8d1d647e81a1
wreckingralph06 <ralphwaldocandaza@gmail.com>
2024-03-01 02:57:13 +0800

    Updated content for Home and Create Pages... 1. Updated Content for Home Page with additional RenderCard component 2. Updated Content for Create Post page with utils for getRandomPrompt data

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+            <h1 className='font-extrabold text-[#222328] text-[32px]'>
+                Create
+            </h1>
+            <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>
+                Create and Imaginative and Visually stunning images 
+                through DALL-E AI and share them with the community 
+            </p>
+        </div>
+    </section>
   )
 }
 
